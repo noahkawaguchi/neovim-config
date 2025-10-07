f739252a0beed4fb8f1a2ef7d350b2930132abe2
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-10-07 11:00:33 -0400

    Move lazy.nvim bootstrap to the top of `init.lua`

--- init.lua
+++ init.lua
@@ -1,3 +1,31 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings
+-- are correct. This is also a good place to set up other settings (vim.opt).
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
 ----------------------------------------------------------------------------------------------------
 -- General config
 ----------------------------------------------------------------------------------------------------
@@ -109,36 +137,8 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 ----------------------------------------------------------------------------------------------------
--- Plugins (lazy.nvim)
+-- Plugins (managed with lazy.nvim)
 ----------------------------------------------------------------------------------------------------
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local out = vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    '--branch=stable',
-    'https://github.com/folke/lazy.nvim.git',
-    lazypath,
-  })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings
--- are correct. This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
 
 -- Set up lazy.nvim
 require('lazy').setup({
